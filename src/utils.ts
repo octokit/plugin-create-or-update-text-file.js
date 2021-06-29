4ad7b7ef9c9de8f4897a1172abb8abf8fbaf79c7
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-06-29 11:05:26 -0700

    fix: make it work in browsers (#30)

--- src/utils.ts
+++ src/utils.ts
@@ -15,20 +15,42 @@ const isNode =
   globalThis.process.release &&
   globalThis.process.release.name;
 
-function browserUtf8ToBase64(data: string) {
-  return btoa(unescape(encodeURIComponent(data)));
-}
-
-function browserBase64ToUtf8(data: string) {
-  return decodeURIComponent(escape(atob(data)));
-}
-
 function nodeUtf8ToBase64(data: string) {
   return Buffer.from(data, "utf-8").toString("base64");
 }
 
 function nodeBase64ToUtf8(data: string) {
   return Buffer.from(data, "base64").toString("utf-8");
+}
+
+// browserUtf8ToBase64 & browserBase64ToUtf8
+// (c) Brandon Rylow - CC BY-SA 4.0
+// https://stackoverflow.com/a/30106551/206879
+function browserUtf8ToBase64(data: string) {
+  // first we use encodeURIComponent to get percent-encoded UTF-8,
+  // then we convert the percent encodings into raw bytes which
+  // can be fed into btoa.
+  return btoa(
+    encodeURIComponent(data).replace(
+      /%([0-9A-F]{2})/g,
+      function toSolidBytes(_match, p1) {
+        // @ts-expect-error - we know what we are doing here
+        return String.fromCharCode("0x" + p1);
+      }
+    )
+  );
+}
+
+function browserBase64ToUtf8(data: string) {
+  // Going backwards: from bytestream, to percent-encoding, to original string.
+  return decodeURIComponent(
+    atob(data)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
 }
 
 export const utf8ToBase64 = isNode ? nodeUtf8ToBase64 : browserUtf8ToBase64;
