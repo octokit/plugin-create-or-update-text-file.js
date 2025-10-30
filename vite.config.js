15c573cca1b9351ff2c12ad09a432627a0d8473d
wolfy1339 <4595477+wolfy1339@users.noreply.github.com>
2025-10-30 03:30:51 -0400

    Ignore src/utils.ts in test coverage

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ export default defineConfig({
   test: {
     coverage: {
       include: ["src/**/*.ts"],
+      ignore: ["src/utils.ts"],
       reporter: ["html", "text", "json"],
       thresholds: {
         100: true,
